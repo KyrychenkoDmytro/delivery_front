29e42e90c214da234b94525366fe1000cd7a0b55
khardy <khardy.official@gmail.com>
2023-05-27 15:05:35 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URLL,
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
