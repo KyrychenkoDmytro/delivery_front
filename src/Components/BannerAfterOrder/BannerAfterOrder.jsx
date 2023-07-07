ae4758b635a194faba4b9c15da0350e792007ae5
khardy <khardy.official@gmail.com>
2023-07-07 15:23:58 +0300

    fix

--- src/Components/BannerAfterOrder/BannerAfterOrder.jsx
+++ src/Components/BannerAfterOrder/BannerAfterOrder.jsx
@@ -10,7 +10,7 @@ const BannerAfterOrder = () => {
     return (
         <div className="BannerAfterOrder">
                 <div className="BannerAfterOrder__wrap">
-                    <h1 className="title BannerAfterOrder__title">Thank you for your order</h1>
+                    <h1 className="title BannerAfterOrder__title">Thank you for your order!</h1>
                     <div className="BannerAfterOrder__bg"></div>
                 </div>
         </div>
